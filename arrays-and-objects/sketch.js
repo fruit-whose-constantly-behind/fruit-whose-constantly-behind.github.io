40453b55c98672bfcec0e8c0c3aa59b843af3f3d
fruit-whose-constantly-behind <afrukhta.s@gmail.com>
2025-10-21 09:53:12 -0600

    Read about how to code an interactive cursor (sources on js file). Also worked on creating a circle that will follow the cursor (incomplete).

--- arrays-and-objects/sketch.js
+++ arrays-and-objects/sketch.js
@@ -5,14 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//For me: trying to make a mouse pointer that is followed by a trail of small triangles??? start with circles and see how it goes. If there is time make a cute little halloween ghost 
+//https://www.geeksforgeeks.org/javascript/create-an-object-that-follows-the-mouse-pointer-using-p5-js/,
+https://www.jhkinfotech.com/blog/cursor-animations-with-css-javascript-code-snippets, https://speckyboy.com/css-javascript-cursor-effects/
+
+
 let radius = 10;
-let shapes = [];
+let movingTriangle = [];
+let movingCircle = [];
 let tri1x1 = mouseX;
 let tri1y1 = mouseY - radius;
 let tri1x2 = mouseX - radius;
 let tri1y2 = mouseY - radius*2;
 let tri1x3 = mouseX + radius;
 let tri1y3 = mouseY + radius;
+
+let x = 0;
+let y = 0;
 
 
 function setup() {
@@ -22,7 +31,7 @@ function setup() {
 function draw() {
   background(220);
   makeCirclePointer();
-  keyPressed();
+  trailingCircle();
 }
 
 function makeCirclePointer() {
@@ -33,20 +42,36 @@ function makeCirclePointer() {
   triangle(mouseX - radius, mouseY, mouseX - radius*2, mouseY - radius, mouseX - radius*2, mouseY + radius);
 }
 
-function keyPressed() {
-  let moveSpeed = 10;
-  while ( x < windowWidth && y < windowHeight) {
-    triangle(tri1x1, tri1y1, tri1x2, tri1y2, tri1x3, tri1y3);
-    tri1y1 = tri1y1 + moveSpeed;
-    tri1y2 = tri1y2 + moveSpeed;
-    tri1y3 = tri1y3 + moveSpeed;
-  }
+function trailingCircle() {  
+movingTriangle.push({
+  x1: mouseX + radius,
+  y1: mouseY, 
+  x2: mouseX + radius*2,
+  y2: mouseY - radius, 
+  x3: mouseX + radius*2, 
+  y3: mouseY + radius, 
+})
+
+movingCircle.push({
+  xValue: mouseX + radius,
+  yValue: mouseY, 
+})
+movingTriangle.x1 = movingTriangle + 50;
+
+for (let i = 0; i < (windowHeight/50); i = i + 50) {
+  movingCircle.xValue = movingCircle.xValue += (mouseX - x) * 0.04;
+  movingCircle.yValue = movingCircle.yvalue += (mouseY - y) * 0.04;
+  fill(0);
+  circle(movingCircle.xValue, movingCircle.y, 25, 25);
 }
+  //Trailing circle
+  x += (mouseX - x) * 0.04;
+  y += (mouseY - y) * 0.04;
+  fill(0);
+  circle(x, y, 25, 25);
 
-
-
-
-function changeShape() {
-  shapes = [triangle, circle, square];
+  // x += (mouseX - x) * 0.04;
+  // y += (mouseY - y) * 0.04;
+  // fill(0);
+  // circle(x, y, 25, 25);
 }
-
